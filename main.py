65068428dfe2a5d464386375ce5945b9b2d74e4a
GreenOrange44 <swayamchaurasia42003@gmail.com>
2026-01-31 17:08:35 +0530

    Final Version

---
+++ main.py
@@ -0,0 +1,10 @@
+from ReadNeighbor.pipeline.training_pipeline import TrainingPipeline
+
+
+def main():
+    obj = TrainingPipeline()
+    obj.start_training_pipeline()
+
+
+if __name__ == "__main__":
+    main()
